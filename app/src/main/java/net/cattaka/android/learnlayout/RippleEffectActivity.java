4c433f9971a88b521bd6446df1658ccebed7ee16
cattaka <cattaka@mail.cattaka.net>
2014-11-24 18:14:02 +0900

    Add RippleEffectActivity

---
+++ app/src/main/java/net/cattaka/android/learnlayout/RippleEffectActivity.java
@@ -0,0 +1,15 @@
+package net.cattaka.android.learnlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cattaka on 14/11/24.
+ */
+public class RippleEffectActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ripple_effect);
+    }
+}
